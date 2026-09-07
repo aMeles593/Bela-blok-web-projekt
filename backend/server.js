0e6119572326f38ad47e6855089e211bd39524f1
Antonio <antonio.meles44@gmail.com>
2026-09-07 18:09:36 +0200

    Promjena na serveru

--- backend/server.js
+++ backend/server.js
@@ -41,8 +41,8 @@ app.get('/api/db-test', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Backend radi na http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Backend radi na portu ${PORT}`);
 });
